9041b0b6230da9ae4430df76f1b4454e8584bd14
둠팬 <dngur521@kumoh.ac.kr>
2024-11-08 23:38:00 +0900

    중복 제거 및 족보 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,18 @@
         {
             Card1 = new PictureBox();
             CardDealButton = new Button();
+            Card2 = new PictureBox();
+            Card3 = new PictureBox();
+            Card4 = new PictureBox();
+            Card5 = new PictureBox();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)Card1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Card2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Card3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Card4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Card5).BeginInit();
             SuspendLayout();
             // 
             // Card1
@@ -38,37 +49,127 @@
             Card1.Image = Properties.Resources.black_joker;
             Card1.Location = new Point(12, 12);
             Card1.Name = "Card1";
-            Card1.Size = new Size(431, 616);
+            Card1.Size = new Size(227, 326);
             Card1.SizeMode = PictureBoxSizeMode.Zoom;
             Card1.TabIndex = 0;
             Card1.TabStop = false;
             // 
             // CardDealButton
             // 
-            CardDealButton.Location = new Point(12, 634);
+            CardDealButton.Location = new Point(12, 344);
             CardDealButton.Name = "CardDealButton";
-            CardDealButton.Size = new Size(431, 90);
+            CardDealButton.Size = new Size(1159, 49);
             CardDealButton.TabIndex = 1;
             CardDealButton.Text = "카드 분배";
             CardDealButton.UseVisualStyleBackColor = true;
             CardDealButton.Click += OnDeal;
             // 
+            // Card2
+            // 
+            Card2.Image = Properties.Resources.black_joker;
+            Card2.Location = new Point(245, 12);
+            Card2.Name = "Card2";
+            Card2.Size = new Size(227, 326);
+            Card2.SizeMode = PictureBoxSizeMode.Zoom;
+            Card2.TabIndex = 0;
+            Card2.TabStop = false;
+            // 
+            // Card3
+            // 
+            Card3.Image = Properties.Resources.black_joker;
+            Card3.Location = new Point(478, 12);
+            Card3.Name = "Card3";
+            Card3.Size = new Size(227, 326);
+            Card3.SizeMode = PictureBoxSizeMode.Zoom;
+            Card3.TabIndex = 0;
+            Card3.TabStop = false;
+            // 
+            // Card4
+            // 
+            Card4.Image = Properties.Resources.black_joker;
+            Card4.Location = new Point(711, 12);
+            Card4.Name = "Card4";
+            Card4.Size = new Size(227, 326);
+            Card4.SizeMode = PictureBoxSizeMode.Zoom;
+            Card4.TabIndex = 0;
+            Card4.TabStop = false;
+            // 
+            // Card5
+            // 
+            Card5.Image = Properties.Resources.black_joker;
+            Card5.Location = new Point(944, 12);
+            Card5.Name = "Card5";
+            Card5.Size = new Size(227, 326);
+            Card5.SizeMode = PictureBoxSizeMode.Zoom;
+            Card5.TabIndex = 0;
+            Card5.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 20F);
+            label1.Location = new Point(12, 428);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 62);
+            label1.TabIndex = 2;
+            label1.Text = "종류";
+            label1.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 10F);
+            label3.Location = new Point(12, 396);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 32);
+            label3.TabIndex = 2;
+            label3.Text = "value";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 10F);
+            label4.Location = new Point(12, 489);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 32);
+            label4.TabIndex = 2;
+            label4.Text = "suit";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(455, 736);
+            ClientSize = new Size(1180, 534);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
             Controls.Add(CardDealButton);
+            Controls.Add(Card5);
+            Controls.Add(Card4);
+            Controls.Add(Card3);
+            Controls.Add(Card2);
             Controls.Add(Card1);
             Name = "Form1";
             Text = "카드 딜러";
             ((System.ComponentModel.ISupportInitialize)Card1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Card2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Card3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Card4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Card5).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox Card1;
         private Button CardDealButton;
+        private PictureBox Card2;
+        private PictureBox Card3;
+        private PictureBox Card4;
+        private PictureBox Card5;
+        private Label label1;
+        private Label label3;
+        private Label label4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,35 +1,189 @@
+using System.Security.Policy;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace G24W10WFCardDealer
 {
     public partial class Form1 : Form
     {
+
+        private const int CardCount = 5;
+        private const int value = 0;
+        private const int suit = 1;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void OnDeal(object sender, EventArgs e)
+        public static string ValueGenerator()
+        {
+            string[] values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+
+            Random random = new();
+            int index = random.Next(0, values.Length);
+
+            return values[index];
+        }
+
+        public static string SuitGenerator()
         {
             string[] suits = ["spades", "diamonds", "hearts", "clubs"];
-            string[] values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
-            int cardval = suits.Length * values.Length;
 
-            Random random = new Random();
-            int card = random.Next(cardval);
+            Random random = new();
+            int index = random.Next(0, suits.Length);
 
-            string suit = suits[card / values.Length];
-            string value = values[card % values.Length];
+            return suits[index];
+        }
 
-            //if (value == "jack" || value == "queen" || value == "king") {
-            //    suit += "2";
-            //}
-            if (value is "jack" or "queen" or "king") {
-                suit += "2";
+        public string[] GetSplit(string[] cardList, int index)
+        {
+            string[] temp = cardList[index].Split(new string[] { "_of_" }, StringSplitOptions.None);
+            string[] spilitstr = [$"{temp[value]}", $"{temp[suit]}"];
+
+            return spilitstr;
+        }
+
+        private void OnDeal(object sender, EventArgs e)
+        {
+            int index = 0;
+            HashSet<string> cardSet = new HashSet<string>();
+
+            while (cardSet.Count < CardCount)
+            {
+                string value = ValueGenerator();
+                string suit  = SuitGenerator();
+
+                if (value is "jack" or "queen" or "king")
+                    suit += "2";
+
+                cardSet.Add($"{value}_of_{suit}");
             }
 
-            Card1.Image = Properties.Resources
-                        .ResourceManager
-                        .GetObject($"{value}_of_{suit}") 
-                        as Image;
+            string[] cardList = cardSet.ToArray();
+
+            Card1.Image = Properties.Resources.ResourceManager.GetObject($"{cardList[index]}") as Image;
+            string[] card1arr = GetSplit(cardList, index);
+            index++;
+            Card2.Image = Properties.Resources.ResourceManager.GetObject($"{cardList[index]}") as Image;
+            string[] card2arr = GetSplit(cardList, index);
+            index++;
+            Card3.Image = Properties.Resources.ResourceManager.GetObject($"{cardList[index]}") as Image;
+            string[] card3arr = GetSplit(cardList, index);
+            index++;
+            Card4.Image = Properties.Resources.ResourceManager.GetObject($"{cardList[index]}") as Image;
+            string[] card4arr = GetSplit(cardList, index);
+            index++;
+            Card5.Image = Properties.Resources.ResourceManager.GetObject($"{cardList[index]}") as Image;
+            string[] card5arr = GetSplit(cardList, index);
+
+            string[][] cardsarr = [card1arr, card2arr, card3arr, card4arr, card5arr];
+
+            string[] cardsvalue = [cardsarr[0][value], 
+                                   cardsarr[1][value], 
+                                   cardsarr[2][value],
+                                   cardsarr[3][value],
+                                   cardsarr[4][value]];
+
+            string[] cardssuit  = [cardsarr[0][suit],
+                                   cardsarr[1][suit],
+                                   cardsarr[2][suit],
+                                   cardsarr[3][suit],
+                                   cardsarr[4][suit]];
+
+            //테스트용 값
+            //cardsvalue = ["10", "jack", "queen", "king", "ace"];
+            //cardsvalue = ["ace", "2", "3", "4", "5"];
+            //cardsvalue = ["ace", "2", "3", "4", "5"];
+            //cardsvalue = ["7", "7", "3", "7", "7"];
+            //cardsvalue = ["7", "7", "3", "7", "3"];
+
+            Dictionary<string, int> valueCounts = new Dictionary<string, int>();
+
+            foreach (var value in cardsvalue)
+            {
+                if (valueCounts.ContainsKey(value))
+                    valueCounts[value]++;
+                else
+                    valueCounts[value] = 1;
+            }
+
+            int pairs = 0;
+            int triples = 0;
+            int quads = 0;
+
+            foreach (var count in valueCounts.Values) {
+                if (count == 2) {
+                    pairs++;
+                    if (count == 4)
+                        pairs++; 
+                }
+                    
+                else if (count == 3)
+                    triples++;
+
+                else if (count == 4)
+                    quads++;
+            }
+
+            bool straight      = false;
+            bool backstraight  = false;
+            bool royalstraight = false;
+
+            if (pairs == 1 && triples == 0)
+                label1.Text = "원페어";
+            else if (pairs == 2)
+                label1.Text = "투페어";
+            else if (triples == 1 && pairs == 0)
+                label1.Text = "트리플";
+            else if (triples == 1 && pairs == 1)
+                label1.Text = "풀하우스";
+            else if (quads == 1)
+                label1.Text = "포카드";
+            else if (cardsvalue[0] == "10"  && cardsvalue[1] == "jack" && cardsvalue[2] == "queen" && cardsvalue[3] == "king"  && cardsvalue[4] == "ace")
+                label1.Text = "마운틴";
+            else if (cardsvalue[0] == "9"   && cardsvalue[1] == "10"   && cardsvalue[2] == "jack"  && cardsvalue[3] == "queen" && cardsvalue[4] == "king")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "8"   && cardsvalue[1] == "9"    && cardsvalue[2] == "10"    && cardsvalue[3] == "jack"  && cardsvalue[4] == "queen")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "7"   && cardsvalue[1] == "8"    && cardsvalue[2] == "9"     && cardsvalue[3] == "10"    && cardsvalue[4] == "jack")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "6"   && cardsvalue[1] == "7"    && cardsvalue[2] == "8"     && cardsvalue[3] == "9"     && cardsvalue[4] == "10")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "5"   && cardsvalue[1] == "6"    && cardsvalue[2] == "7"     && cardsvalue[3] == "8"     && cardsvalue[4] == "9")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "4"   && cardsvalue[1] == "5"    && cardsvalue[2] == "6"     && cardsvalue[3] == "7"     && cardsvalue[4] == "8")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "3"   && cardsvalue[1] == "4"    && cardsvalue[2] == "5"     && cardsvalue[3] == "6"     && cardsvalue[4] == "7")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "2"   && cardsvalue[1] == "3"    && cardsvalue[2] == "4"     && cardsvalue[3] == "5"     && cardsvalue[4] == "6")
+                label1.Text = "스트레이트";
+            else if (cardsvalue[0] == "ace" && cardsvalue[1] == "2"    && cardsvalue[2] == "3"     && cardsvalue[3] == "4"     && cardsvalue[4] == "5")
+                label1.Text = "백스트레이트";
+            else
+                label1.Text = "기타";
+
+            if (label1.Text == "스트레이트")
+                straight = true;
+            else if (label1.Text == "마운틴")
+                royalstraight = true;
+            else if (label1.Text == "백스트레이트")
+                backstraight  = true;
+
+            //테스트용 값
+            //cardssuit = ["a", "a", "a", "a", "a"];
+
+            if ((cardssuit[0] == cardssuit[1]) && (cardssuit[1] == cardssuit[2]) && (cardssuit[2] == cardssuit[3]) && (cardssuit[3] == cardssuit[4]) && (cardssuit[4] == cardssuit[0]))
+            {
+                if (straight == true)
+                    label1.Text = "스트레이트 플러쉬";
+                else if (royalstraight == true)
+                    label1.Text = "로얄스트레이트 플러쉬";
+                else if (backstraight  == true)
+                    label1.Text = "백스트레이트 플러쉬";
+                else
+                    label1.Text = "플러쉬";
+            }
+            label3.Text = $"{cardsvalue[0]}, {cardsvalue[1]}, {cardsvalue[2]}, {cardsvalue[3]}, {cardsvalue[4]}";
+            label4.Text = $"{cardssuit[0]},  {cardssuit[1]},  {cardssuit[2]},  {cardssuit[3]},  {cardssuit[4]}"; 
         }
     }
 }
